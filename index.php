e466d9209edba1dce0d63fe95be8f59c3d197030
stefaneechua <stefaneechua@gmail.com>
2018-01-12 03:10:47 +1300

    finished home

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<!DOCTYPE html>
+
 <?php  ?>
 
-<!DOCTYPE html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -14,7 +16,7 @@
  	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
    <!-- Custom Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Heebo:100" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet">
     <script src="https://use.fontawesome.com/48437d1460.js"></script>
 
@@ -27,6 +29,46 @@
   </head>
 
   <body>
-    
+
+    <div class="wrapperHome">
+      <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#" style="color:white;">keen.</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li><a href="#">Central</a></li>
+            <li><a href="#">East</a></li>
+            <li><a href="#">West</a></li>
+            <li><a href="#">South</a></li>
+            <li><a href="#">North</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Admin Login</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <div class="wrapperBG container-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <h1 class="display-1 keenHeader">
+              Ready to<br>gain some<br>experience?<br>
+              </h1>
+              <h2 class="display-1 keenHeader2">keen.</h2>
+            </div>
+            <div class="col-lg-6 keenImg">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <div class="wrapperContact">
+
+    </div>
+
   </body>
 </html>
