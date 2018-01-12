0021151207f69de8a0c1fb7c1739454ff285072d
stefaneechua <stefaneechua@gmail.com>
2018-01-12 20:53:10 +1300

    added footer styles, social icons

--- index.php
+++ index.php
@@ -92,7 +92,7 @@
                 <textarea placeholder="Message" tabindex="4" required></textarea>
               </fieldset>
               <fieldset>
-                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                <button name="submit" type="submit" id="contact-submit">Send</button>
               </fieldset>
             </form>
           </div>
@@ -101,8 +101,34 @@
     </div>
 
     <footer>
-      <p class="text-center">Stefanee Chua &nbsp; | &nbsp; &copy; 2018</p>
-      <p class="text-center keenFooter">keen.</p>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p>Stefanee Chua &nbsp; | &nbsp; &copy; 2018</p>
+            <p class="keenFooter">keen.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+  		  		<div class="social">
+  		  			<ul class="list-inline social-icons text-center">
+  		  				<li><a href="https://www.behance.net/stefaneechua">
+  		  					<i class="fa fa-behance fa-lg"></i>
+  		  				</a></li>
+  		  				<li><a href="https://www.instagram.com/stefaneeux">
+  		  					<i class="fa fa-instagram fa-lg"></i>
+  		  				</a></li>
+  		  				<li><a href="http://bit.ly/2xDn5Ok">
+  		  					<i class="fa fa-youtube-play fa-lg"></i>
+  		  				</a></li>
+  		  				<li><a href="https://linkedin.com/in/stefaneechua">
+  		  					<i class="fa fa-linkedin fa-lg"></i>
+  		  				</a></li>
+  		  			</ul>
+  		  		</div>
+  		  	</div>
+        </div>
+      </div>
     </footer>
 
   </body>
